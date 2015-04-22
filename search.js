a90aeb73f0119283db004ac44da1304eabe8104e
Nikhil Modak <mod.nikhil@gmail.com>
2015-04-22 10:53:12 -0500

    Minor Changes

--- search.js
+++ search.js
@@ -5,7 +5,7 @@
  */
 // After the API loads, call a function to enable the search box.
 function handleAPILoaded() {
-  $('#search-button').attr('disabled', false);
+  $('#search').attr('disabled', false);
 }
 
 // Search for a specified string.
